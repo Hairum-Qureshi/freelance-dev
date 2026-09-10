4c7f94c7e57fd5855e63b6606f69edc01040f513
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-09 23:30:04 -0400

    added Body component

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,33 +1,15 @@
 import GoogleOAuthButton from "../components/GoogleOAuthButton";
+import Header from "../components/home-page/Header";
+import Body from "../components/home-page/Body";
 import { useCurrentUser } from "../hooks/useCurrentUser";
 
 export default function Home() {
-  const { data: currUserData } = useCurrentUser();
+	const { data: currUserData } = useCurrentUser();
 
-  return (
-    <div className="min-h-screen max-h-auto p-10 bg-white relative overflow-hidden flex flex-col">
-      <h1 className="text-3xl font-semibold text-black">Hello</h1>
-      <p className="text-lg font-semibold my-5">
-        If you're not seeing the Google OAuth button, it's due to the{" "}
-        <code className="bg-gray-100 text-base text-green-600 p-1 rounded">
-          /api/auth/current-user
-        </code>{" "}
-        API endpoint returning HTML so technically{" "}
-        <code className="bg-gray-100 text-base text-green-600 p-1 rounded">
-          currUserData
-        </code>{" "}
-        isn't undefined. Once you provide the appropriate backend{" "}
-        <code className="bg-gray-100 text-base text-green-600 p-1 rounded">
-          env
-        </code>{" "}
-        variables and the appropriate data, the endpoint will work and you'll
-        see the button below.
-      </p>
-     
-
-      <div className="flex flex-col items-center gap-4">
-        {!currUserData && <GoogleOAuthButton />}
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full min-h-screen max-h-auto bg-slate-100/50 p-3">
+			<Header />
+			<Body />
+		</div>
+	);
 }
