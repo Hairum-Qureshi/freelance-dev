7c7fa0af2933703992cbb743f5992c8e7382f4f9
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-09 21:24:55 -0400

    added Navbar component

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -4,19 +4,21 @@ import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import "../css/index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import Navbar from "./Navbar";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <GoogleOAuthProvider
-        clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}
-      >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </GoogleOAuthProvider>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<GoogleOAuthProvider
+				clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}
+			>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</GoogleOAuthProvider>
+		</BrowserRouter>
+	);
 }
