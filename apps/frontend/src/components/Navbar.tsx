c9f568af9f988f3b2dc0bcb246376a54d469e184
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-09 21:24:36 -0400

    created Navbar component

---
+++ apps/frontend/src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import freelanceDevLogo from "../assets/freelance-dev-logo.jpeg";
+export default function Navbar() {
+	return (
+		<div className="w-full h-16 bg-slate-100/50 text-black flex items-center px-5">
+			<div className="flex items-center">
+				<img
+					src={freelanceDevLogo}
+					alt="Freelance Dev Logo"
+					className="h-22 w-22 object-cover"
+				/>
+				<h1 className="font-semibold text-2xl">Freelance Dev</h1>
+			</div>
+			<div className="ml-auto space-x-3">
+				<Link to="/about" className="hover:underline">
+					About
+				</Link>
+				<Link to="/contact" className="hover:underline">
+					Contact
+				</Link>
+				<Link to="/services" className="hover:underline">
+					Services
+				</Link>
+				<button className="bg-black text-white px-3 py-1.5 rounded-md hover:cursor-pointer hover:bg-gray-800 active:bg-gray-900">
+					Join Now
+				</button>
+			</div>
+		</div>
+	);
+}
