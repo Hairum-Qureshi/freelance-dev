16af7aa844777affa0c6370dd70bf022120a6b2a
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-10 13:41:37 -0400

    tweaked Google account data retrieval logic using access token

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,10 +1,12 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { OAuth2Client } from 'google-auth-library';
 import { JwtService } from '@nestjs/jwt';
 import { UserPayload } from '../types';
+import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { neon } from '@neondatabase/serverless';
 import { SnowflakeId } from 'snowflake-id';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class AuthService {
@@ -13,6 +15,7 @@ export class AuthService {
     @Inject('GoogleOAuthClient') private googleOAuthClient: OAuth2Client,
     private configService: ConfigService,
     @Inject('NeonDBProvider') private sql: ReturnType<typeof neon>,
+    private httpService: HttpService,
   ) {}
 
   getAuthCookieOptions() {
@@ -23,14 +26,27 @@ export class AuthService {
     };
   }
 
-  async googleAuth(token: string): Promise<{ jwtToken: string }> {
-    const ticket = await this.googleOAuthClient.verifyIdToken({
-      idToken: token,
-      audience: this.configService.get<string>('GOOGLE_OAUTH_CLIENT_ID'),
-    });
+  async googleAuth(accessToken: string): Promise<{ jwtToken: string }> {
+    const response = await firstValueFrom(
+      this.httpService.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }),
+    );
 
-    const { email, picture, given_name, family_name } =
-      ticket.getPayload() || {};
+    if (!response) {
+      throw new UnauthorizedException('Invalid Google access token');
+    }
+
+    const googleUser = (await response.data) as {
+      email: string;
+      picture: string;
+      given_name: string;
+      family_name: string;
+    };
+
+    const { email, picture, given_name, family_name } = googleUser;
 
     let [user] = (await this
       .sql`SELECT * FROM users WHERE email = ${email}`) as UserPayload[];
@@ -45,9 +61,7 @@ export class AuthService {
         .sql`INSERT INTO users (id, first_name, last_name, email, profile_picture) VALUES (${snowflake.generate()}, ${given_name || 'GoogleUser'}, ${family_name || 'GoogleUser'}, ${email}, ${picture}) RETURNING *`) as UserPayload[];
     }
 
-    const jwtToken = this.jwtService.sign({
-      _id: user.id,
-    });
+    const jwtToken = this.jwtService.sign({ id: user.id });
 
     return { jwtToken };
   }
