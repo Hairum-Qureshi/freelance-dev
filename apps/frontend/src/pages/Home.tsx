6d379006a7a6ded0550d4f704fc36434879f9573
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-10 00:00:38 -0400

    removed unused imports and code

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,11 +1,7 @@
-import GoogleOAuthButton from "../components/GoogleOAuthButton";
 import Header from "../components/home-page/Header";
 import Body from "../components/home-page/Body";
-import { useCurrentUser } from "../hooks/useCurrentUser";
 
 export default function Home() {
-	const { data: currUserData } = useCurrentUser();
-
 	return (
 		<div className="w-full min-h-screen max-h-auto bg-slate-100/50 p-3">
 			<Header />
