8ffb71a539edc9c9d80432a4411c84c767d872ad
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-10 00:00:05 -0400

    changed Google sign in button with custom button and updated login logic

--- apps/frontend/src/components/GoogleOAuthButton.tsx
+++ apps/frontend/src/components/GoogleOAuthButton.tsx
@@ -1,16 +1,23 @@
-import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
 import useGoogleAuth from "../hooks/useGoogleAuth";
+import { FaGoogle } from "react-icons/fa";
 
 export default function GoogleOAuthButton() {
-  const { googleSignInMutation } = useGoogleAuth();
-  return (
-    <GoogleLogin
-      onSuccess={(credentialResponse) => {
-        googleSignInMutation(credentialResponse.credential as string);
-      }}
-      onError={() => {
-        console.log("Login Failed");
-      }}
-    />
-  );
+	const { googleSignInMutation } = useGoogleAuth();
+
+	const login = useGoogleLogin({
+		onSuccess: tokenResponse => googleSignInMutation(tokenResponse.access_token)
+	});
+
+	return (
+		<button
+			onClick={() => {
+				login();
+			}}
+			className="w-1/2 rounded-lg bg-black px-4 py-3 font-medium text-white transition"
+		>
+			<FaGoogle className="inline mr-2" />
+			Sign in with Google
+		</button>
+	);
 }
