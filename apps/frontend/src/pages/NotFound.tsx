9032dd270dcea3605eaf131fe7afc02f615338dd
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-11 10:07:27 -0400

    styled NotFound component

--- apps/frontend/src/pages/NotFound.tsx
+++ apps/frontend/src/pages/NotFound.tsx
@@ -1,3 +1,20 @@
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-	return <div>NotFound</div>;
+	return (
+		<div className="h-screen flex items-center justify-center flex-col">
+			<h1 className="text-2xl font-bold">404 - Page Not Found</h1>
+			<p className="text-lg mt-4">
+				The page you are looking for does not exist.
+			</p>
+			<Link
+				to="/"
+				className="mt-4 text-blue-500 hover:underline flex items-center"
+			>
+				<FaArrowLeftLong className="mr-2" />
+				Go back to Home
+			</Link>
+		</div>
+	);
 }
