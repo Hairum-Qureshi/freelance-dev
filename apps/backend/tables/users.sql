d7a7db7c2459050274b66d90b0b28dfdfb171990
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-10 22:07:19 -0400

    updated users SQL table code after adding new columns

--- apps/backend/tables/users.sql
+++ apps/backend/tables/users.sql
@@ -4,6 +4,9 @@ CREATE TABLE users (
     "lastName" TEXT NOT NULL,
     email TEXT NOT NULL,
     "profilePicture" TEXT NOT NULL,
+    "completedOnboarding" BOOLEAN NOT NULL,
+    "onboardingAnswers" JSON,
+    "deleted" BOOLEAN NOT NULL,
     "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL,
     "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
 
