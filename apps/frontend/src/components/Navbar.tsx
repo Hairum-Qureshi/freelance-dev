34e75b18ee2a9ebe625819fafb3485a234647501
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-10 20:34:10 -0400

    added conditional logic to hide the Join button and replace it with the profile button that redirects you to your profile if you've logged in

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { Link, useNavigate } from "react-router-dom";
 import freelanceDevLogo from "../assets/freelance-dev-logo.jpeg";
+import { useCurrentUser } from "../hooks/useCurrentUser";
 
 export default function Navbar() {
 	const navigate = useNavigate();
+	const { data: currUserData } = useCurrentUser();
 
 	return (
 		<div className="w-full h-16 bg-slate-100/50 text-black flex items-center px-5">
@@ -24,12 +26,21 @@ export default function Navbar() {
 				<Link to="/services" className="hover:underline">
 					Services
 				</Link>
-				<button
-					className="bg-black text-white px-3 py-1.5 rounded-md hover:cursor-pointer hover:bg-gray-800 active:bg-gray-900"
-					onClick={() => navigate("/join")}
-				>
-					Join Now
-				</button>
+				{!currUserData ? (
+					<button
+						className="bg-black text-white px-3 py-1.5 rounded-md hover:cursor-pointer hover:bg-gray-800 active:bg-gray-900"
+						onClick={() => navigate("/join")}
+					>
+						Join Now
+					</button>
+				) : (
+					<button
+						className="bg-black text-white px-3 py-1.5 rounded-md hover:cursor-pointer hover:bg-gray-800 active:bg-gray-900"
+						onClick={() => navigate(`/p/${currUserData.id}`)}
+					>
+						View Profile
+					</button>
+				)}
 			</div>
 		</div>
 	);
