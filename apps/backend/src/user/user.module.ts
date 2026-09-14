7febf24885af9176a06c6db539b35ff9f935d758
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-14 18:21:40 -0400

    imported ImageKitProvider in providers array

--- apps/backend/src/user/user.module.ts
+++ apps/backend/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { NeonDBProvider } from 'src/config/postgres-db';
+import { ImageKitProvider } from 'src/config/imagekit';
 
 @Module({
-  providers: [UserService, NeonDBProvider],
+  providers: [UserService, NeonDBProvider, ImageKitProvider],
   controllers: [UserController],
 })
 export class UserModule {}
