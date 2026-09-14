f5121e9e00ff9766a6e6c864a0d1f054473a8b46
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-14 19:44:37 -0400

    added PATCH endpoint for updating user's data by uploading resume

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  UploadedFile,
+  Controller,
+  Post,
+  UseGuards,
+  Patch,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CurrentUser } from 'src/decorators/currentUser.decorator';
 import type { UserPayload } from 'src/types';
 import { OnboardingDTO } from 'src/DTOs/onboarding.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('user')
 export class UserController {
@@ -19,5 +28,15 @@ export class UserController {
       currentUser.id,
       onboardingData,
     );
+  }
+
+  @Patch('/attach-resume')
+  @UseGuards(AuthGuard())
+  @UseInterceptors(FileInterceptor('resume'))
+  attachResume(
+    @CurrentUser() currentUser: UserPayload,
+    @UploadedFile() resume: Express.Multer.File,
+  ) {
+    return this.userService.attachResume(currentUser.id, resume);
   }
 }
