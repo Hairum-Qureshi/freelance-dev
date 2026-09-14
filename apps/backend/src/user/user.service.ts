56784784c064201dde975d60c68606f3fa4f4945
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-14 19:44:14 -0400

    added attachResume method for uploading a user's resume to ImageKit and updating their user data by storing the resume's URL

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -1,14 +1,35 @@
 import { neon } from '@neondatabase/serverless';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Inject } from '@nestjs/common';
+import type ImageKit from 'imagekit';
 import { OnboardingDTO } from 'src/DTOs/onboarding.dto';
 
 @Injectable()
 export class UserService {
-  constructor(@Inject('NeonDBProvider') private sql: ReturnType<typeof neon>) {}
+  constructor(
+    @Inject('NeonDBProvider') private sql: ReturnType<typeof neon>,
+    @Inject('ImageKitProvider') private readonly imageKit: ImageKit,
+  ) {}
 
   async submitOnboardingAnswers(userID: string, onboardingData: OnboardingDTO) {
     await this
       .sql`UPDATE users SET "onboardingAnswers" = ${onboardingData}, "completedOnboarding" = true WHERE id = ${userID}`;
+  }
+
+  async attachResume(userID: string, resume: Express.Multer.File) {
+    const resumeData = await this.imageKit.upload({
+      file: resume.buffer,
+      folder: `/profiles/${userID}`,
+      fileName: resume.originalname,
+    });
+
+    if (!resumeData)
+      throw new HttpException(
+        'Failed to upload resume',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+
+    await this
+      .sql`UPDATE users SET resume_url = ${resumeData.url} WHERE id = ${userID}`;
   }
 }
