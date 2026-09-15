1c3154eaeec96c8b98cb167f47dacd5877fa0a88
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-15 13:32:25 -0400

    made no significant change

---
+++ apps/frontend/src/components/home-page/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+	return (
+		<div className="text-black p-4 w-full">
+			<div className="w-5/6 m-auto my-10 space-y-2">
+				<h1 className="text-7xl font-semibold w-3/4">
+					Your first freelance job starts here.
+				</h1>
+				<h3 className="text-xl w-3/4">
+					A freelance marketplace built for early-career web developers. Find
+					smaller projects, work with real clients, and build the track record
+					you need to take the next step.
+				</h3>
+			</div>
+		</div>
+	);
+}
