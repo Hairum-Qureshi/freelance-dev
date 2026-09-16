52acfced397d5a6160fd977b5e906bdaf92027bf
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-16 00:46:48 -0400

    updated UserPayload import

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -10,9 +10,9 @@ import {
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport/dist/auth.guard';
 import { CurrentUser } from '../decorators/currentUser.decorator';
-import * as types from '../types';
 import express from 'express';
 import type { Response } from 'express';
+import type { UserPayload } from '@repo/shared-types';
 import { BearerToken } from 'src/decorators/bearerToken.decorator';
 import { RealIP } from 'nestjs-real-ip';
 
@@ -52,7 +52,7 @@ export class AuthController {
 
   @Get('current-user')
   @UseGuards(AuthGuard())
-  getCurrentUser(@CurrentUser() user: types.UserPayload): types.UserPayload {
+  getCurrentUser(@CurrentUser() user: UserPayload): UserPayload {
     return this.authService.getCurrentUser(user);
   }
 }
