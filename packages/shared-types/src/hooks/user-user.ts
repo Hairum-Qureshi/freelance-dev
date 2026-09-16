fdcb4a4738d6b4722743b9e4b91f097455b0c47e
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-16 10:10:24 -0400

    created use-user hook interface file

---
+++ packages/shared-types/src/hooks/user-user.ts
@@ -0,0 +1,13 @@
+import type { UseMutationResult } from "@tanstack/react-query";
+import { OnboardingData } from "../types/onboarding-data.js";
+
+export interface UseUserHook {
+	onboardingMutation: UseMutationResult<
+		void,
+		Error,
+		{ onboardingData: OnboardingData },
+		unknown
+	>;
+	attachResumeMutation: UseMutationResult<void, Error, { file: File }, unknown>;
+	removeResumeMutation: UseMutationResult<void, Error, void, unknown>;
+}
