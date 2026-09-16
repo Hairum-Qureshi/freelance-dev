4e2be72ec4033c0b9d645b9e5c58b10e2078af20
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-16 11:34:49 -0400

    replaced instances of 'resumeUrl' with 'resumeId'

--- apps/frontend/src/components/profile/ProfileContent.tsx
+++ apps/frontend/src/components/profile/ProfileContent.tsx
@@ -54,7 +54,7 @@ export default function ProfileContent({
 						Edit profile
 					</button>
 
-					{currUserData?.resumeUrl ? (
+					{currUserData?.resumeId ? (
 						<button
 							type="button"
 							className="rounded-md border border-slate-300 px-4 py-2 text-sm font-medium text-slate-700 hover:cursor-pointer hover:bg-slate-50"
