aec0bdf9165e2fb5fe063cc8a3a2d4966390f7a3
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-16 18:14:03 -0400

    added GET endpoint for getting all user data by ID and also converted all currUserIDs into BigInts that are being passed into their respected methods

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -6,6 +6,8 @@ import {
   UseGuards,
   Patch,
   UseInterceptors,
+  Delete,
+  Get,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CurrentUser } from 'src/decorators/currentUser.decorator';
@@ -13,10 +15,17 @@ import type { UserPayload } from '@repo/shared-types';
 import { OnboardingDTO } from 'src/DTOs/onboarding.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Param } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('/:userId/profile')
+  @UseGuards(AuthGuard())
+  getUserProfile(@Param('userId') userId: string) {
+    return this.userService.getUserProfile(BigInt(userId));
+  }
 
   @Post('onboarding/answers')
   @UseGuards(AuthGuard())
@@ -25,7 +34,7 @@ export class UserController {
     @Body() onboardingData: OnboardingDTO,
   ) {
     return this.userService.submitOnboardingAnswers(
-      currentUser.id,
+      BigInt(currentUser.id),
       onboardingData,
     );
   }
@@ -37,6 +46,12 @@ export class UserController {
     @CurrentUser() currentUser: UserPayload,
     @UploadedFile() resume: Express.Multer.File,
   ) {
-    return this.userService.attachResume(currentUser.id, resume);
+    return this.userService.attachResume(BigInt(currentUser.id), resume);
+  }
+
+  @Delete('/remove-resume')
+  @UseGuards(AuthGuard())
+  removeResume(@CurrentUser() currentUser: UserPayload) {
+    return this.userService.removeResume(BigInt(currentUser.id));
   }
 }
