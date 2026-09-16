561bcd3b2e406131e0e1369d70e60039b68f29e9
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-16 18:14:32 -0400

    updated user ID types from string to bigint and removed bigint conversions since they're already converted and also added getUserProfile method

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -13,17 +13,17 @@ export class UserService {
     @Inject('ImageKitProvider') private readonly imageKit: ImageKit,
   ) {}
 
-  async submitOnboardingAnswers(userID: string, onboardingData: OnboardingDTO) {
+  async submitOnboardingAnswers(userID: bigint, onboardingData: OnboardingDTO) {
     await this.db
       .update(usersTable)
       .set({
         onboarding_answers: onboardingData,
         completed_onboarding: true,
       })
-      .where(eq(usersTable.id, BigInt(userID)));
+      .where(eq(usersTable.id, userID));
   }
 
-  async attachResume(userID: string, resume: Express.Multer.File) {
+  async attachResume(userID: bigint, resume: Express.Multer.File) {
     if (!resume)
       throw new HttpException(
         'No resume file provided',
@@ -31,18 +31,19 @@ export class UserService {
       );
 
     const resumeExists = await this.db
-      .select({ resume_url: usersTable.resume_url })
+      .select({ resume_id: usersTable.resume_id })
       .from(usersTable)
-      .where(eq(usersTable.id, BigInt(userID)))
+      .where(eq(usersTable.id, userID))
       .limit(1);
 
-    if (resumeExists[0]?.resume_url)
+    if (resumeExists[0]?.resume_id)
       throw new HttpException('Resume already exists', HttpStatus.BAD_REQUEST);
 
     const resumeData = await this.imageKit.upload({
       file: resume.buffer,
       folder: `/profiles/${userID}`,
       fileName: 'Resume.pdf',
+      useUniqueFileName: false,
     });
 
     if (!resumeData)
@@ -54,8 +55,44 @@ export class UserService {
     await this.db
       .update(usersTable)
       .set({
-        resume_url: resumeData.url,
+        resume_id: resumeData.fileId,
       })
-      .where(eq(usersTable.id, BigInt(userID)));
+      .where(eq(usersTable.id, userID));
+  }
+
+  async removeResume(userID: bigint) {
+    const [resumeExists] = await this.db
+      .select({ resume_id: usersTable.resume_id })
+      .from(usersTable)
+      .where(eq(usersTable.id, userID));
+
+    if (!resumeExists?.resume_id)
+      throw new HttpException('No resume to remove', HttpStatus.NOT_FOUND);
+
+    await this.imageKit.deleteFile(resumeExists.resume_id);
+
+    await this.db
+      .update(usersTable)
+      .set({
+        resume_id: null,
+      })
+      .where(eq(usersTable.id, userID));
+
+    return { success: true };
+  }
+
+  async getUserProfile(userId: bigint) {
+    const [user] = await this.db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, userId))
+      .limit(1);
+
+    if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+
+    return {
+      ...user,
+      id: user.id.toString(), // need to convert 'id' to string because JSON doesn't support 'bigint'
+    };
   }
 }
