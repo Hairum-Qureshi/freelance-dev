2014ad288379f9b668caf69e0ff4c38d3093ef00
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-17 19:10:32 -0400

    removed 'hiring_title' from UserPayload type

--- packages/shared-types/src/types/user.ts
+++ packages/shared-types/src/types/user.ts
@@ -8,7 +8,6 @@ export type UserPayload = {
 	profile_picture: string;
 	completed_onboarding: boolean;
 	onboarding_answers: OnboardingAnswers;
-	hiring_title: string;
 	resume_id: string;
 	location: string;
 	deleted: boolean;
