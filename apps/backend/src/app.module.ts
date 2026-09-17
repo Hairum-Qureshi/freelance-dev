761a8b5874a1e0185d06820b1b029bae8c1d96cd
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-17 15:35:59 -0400

    added chat module to imports array

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
       isGlobal: true,
     }),
     UserModule,
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
