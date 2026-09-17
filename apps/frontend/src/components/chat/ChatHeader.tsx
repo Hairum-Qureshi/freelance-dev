583544a9c205417b512487b383d5d2dda6b23045
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-17 19:24:04 -0400

    replaced hard coded content with dynamic prop data

--- apps/frontend/src/components/chat/ChatHeader.tsx
+++ apps/frontend/src/components/chat/ChatHeader.tsx
@@ -1,14 +1,23 @@
-export default function ChatHeader() {
+export default function ChatHeader({
+	profilePicture,
+	name,
+	title
+}: {
+	profilePicture: string;
+	name: string;
+	title: string;
+}) {
 	return (
 		<div className="border-b border-slate-200 p-3 flex items-center h-[65px] shrink-0">
 			<img
-				src="https://i.pinimg.com/236x/1d/ec/e2/1dece2c8357bdd7cee3b15036344faf5.jpg?nii=t"
+				src={profilePicture}
 				alt="Message"
+				referrerPolicy="no-referrer"
 				className="h-10 w-10 rounded-full object-cover border-2 border-slate-200"
 			/>
 			<div className="ml-3">
-				<p className="font-semibold text-base leading-tight">John Doe</p>
-				<p className="text-sm text-gray-500">Product Manager</p>
+				<p className="font-semibold text-base leading-tight">{name}</p>
+				<p className="text-sm text-gray-500">{title}</p>
 			</div>
 		</div>
 	);
