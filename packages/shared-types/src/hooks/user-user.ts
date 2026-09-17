fd751ab88f36d504d40ac391fcd5fef944ba603d
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-16 22:47:44 -0400

    added userProfileData param to UseUserHook interface

--- packages/shared-types/src/hooks/user-user.ts
+++ packages/shared-types/src/hooks/user-user.ts
@@ -1,5 +1,6 @@
 import type { UseMutationResult } from "@tanstack/react-query";
 import { OnboardingData } from "../types/onboarding-data.js";
+import { UserPayload } from "../types/user.js";
 
 export interface UseUserHook {
 	onboardingMutation: UseMutationResult<
@@ -10,4 +11,5 @@ export interface UseUserHook {
 	>;
 	attachResumeMutation: UseMutationResult<void, Error, { file: File }, unknown>;
 	removeResumeMutation: UseMutationResult<void, Error, void, unknown>;
+	userProfileData: UserPayload;
 }
