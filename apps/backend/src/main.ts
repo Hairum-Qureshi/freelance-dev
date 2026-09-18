671eca0e53a71f45ffac865cba41d20794bdb505
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-18 00:46:09 -0400

    registered custom bigint interceptor and globalized it

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import { BigIntInterceptor } from './interceptors/bigint.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -21,6 +22,8 @@ async function bootstrap() {
   app.setGlobalPrefix('api', {
     exclude: ['/'],
   });
+
+  app.useGlobalInterceptors(new BigIntInterceptor());
 
   const PORT = configService.get<number>('PORT') ?? 3000;
 
