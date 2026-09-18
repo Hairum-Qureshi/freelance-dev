d88ebe9cb43f53c6563aac9c28d4b9b2016a9602
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-18 00:48:48 -0400

    added interceptor to GET endpoint for getting all chats

--- apps/backend/src/chat/chat.controller.ts
+++ apps/backend/src/chat/chat.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Post, UseGuards, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Body,
+  Get,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateChatDTO } from '../DTOs/chat.dto';
 import { ChatService } from './chat.service';
 import { CurrentUser } from 'src/decorators/currentUser.decorator';
 import type { UserPayload } from '@repo/shared-types';
+import { BigIntInterceptor } from 'src/interceptors/bigint.interceptor';
 
 @Controller('chat')
 export class ChatController {
@@ -17,5 +25,12 @@ export class ChatController {
     @CurrentUser() currentUser: UserPayload,
   ) {
     return this.chatService.createChat(createChatDTO, BigInt(currentUser.id));
+  }
+
+  @Get('all')
+  @UseInterceptors(BigIntInterceptor)
+  @UseGuards(AuthGuard())
+  getAllChats(@CurrentUser() currentUser: UserPayload) {
+    return this.chatService.getAllChats(BigInt(currentUser.id));
   }
 }
