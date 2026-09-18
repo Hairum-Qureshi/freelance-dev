b3bbdb78997638398647eebbec0a98240c1c06a2
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-18 18:40:38 -0400

    changed casing from snake case to camel case

--- apps/backend/src/schema.ts
+++ apps/backend/src/schema.ts
@@ -10,48 +10,48 @@ import type { OnboardingAnswers } from '@repo/shared-types';
 
 export const usersTable = pgTable('users', {
   id: bigint({ mode: 'bigint' }).primaryKey(),
-  first_name: text('first_name').notNull(),
-  last_name: text('last_name').notNull(),
+  firstName: text('first_name').notNull(),
+  lastName: text('last_name').notNull(),
   email: text('email').notNull(),
-  profile_picture: text('profile_picture').notNull(),
-  completed_onboarding: boolean('completed_onboarding').default(false),
-  onboarding_answers: jsonb('onboarding_answers')
+  profilePicture: text('profile_picture').notNull(),
+  completedOnboarding: boolean('completed_onboarding').default(false),
+  onboardingAnswers: jsonb('onboarding_answers')
     .$type<OnboardingAnswers>()
     .notNull(),
-  resume_id: text('resume_id'),
+  resumeId: text('resume_id'),
   location: text('location'),
   deleted: boolean('deleted').default(false),
-  created_at: timestamp('created_at').defaultNow(),
-  updated_at: timestamp('updated_at').defaultNow(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').defaultNow(),
 });
 
 export const chatsTable = pgTable('chats', {
   id: bigint({ mode: 'bigint' }).primaryKey(),
-  created_at: timestamp('created_at').defaultNow(),
-  updated_at: timestamp('updated_at').defaultNow(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').defaultNow(),
 });
 
 export const participantsTable = pgTable('participants', {
   id: bigint({ mode: 'bigint' }).primaryKey(),
-  chat_id: bigint({ mode: 'bigint' })
+  chatId: bigint({ mode: 'bigint' })
     .notNull()
     .references(() => chatsTable.id),
-  user_id: bigint({ mode: 'bigint' })
+  userId: bigint({ mode: 'bigint' })
     .notNull()
     .references(() => usersTable.id),
-  created_at: timestamp('created_at').defaultNow(),
-  updated_at: timestamp('updated_at').defaultNow(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').defaultNow(),
 });
 
 export const messagesTable = pgTable('messages', {
   id: bigint({ mode: 'bigint' }).primaryKey(),
-  chat_id: bigint({ mode: 'bigint' })
+  chatId: bigint({ mode: 'bigint' })
     .notNull()
     .references(() => chatsTable.id),
-  sender_id: bigint({ mode: 'bigint' })
+  senderId: bigint({ mode: 'bigint' })
     .notNull()
     .references(() => usersTable.id),
   message: text('message').notNull(),
-  created_at: timestamp('created_at').defaultNow(),
-  updated_at: timestamp('updated_at').defaultNow(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').defaultNow(),
 });
