095af3b61b5c403bfe768e1cb7f743a901bba3d3
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-17 23:50:18 -0400

    removed BigInt conversions

--- apps/frontend/src/hooks/useChat.ts
+++ apps/frontend/src/hooks/useChat.ts
@@ -8,13 +8,11 @@ export default function useChat() {
 
 	const createChatMutation = useMutation({
 		mutationFn: async ({ message }: { message: string }): Promise<void> => {
-			if (!chatID && !searchParams.get("to")) return;
-
 			await axios.post(
 				`${import.meta.env.VITE_BACKEND_URL}/api/chat/create`,
 				{
-					chatID: BigInt(chatID as string),
-					to: BigInt(BigInt(searchParams.get("to") as string)),
+					chatID,
+					to: searchParams.get("to"),
 					message
 				},
 				{
