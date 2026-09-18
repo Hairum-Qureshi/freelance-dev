031cfd7ccdd0f4a638fad3d5e40ea50018c8aafc
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-17 20:18:43 -0400

    changed 'chatId' to 'chatID

--- apps/backend/src/DTOs/chat.dto.ts
+++ apps/backend/src/DTOs/chat.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateChatDTO {
   @IsString()
   @IsNotEmpty()
-  chatId!: string;
+  chatID!: string;
 
   @IsString()
   @IsNotEmpty()
