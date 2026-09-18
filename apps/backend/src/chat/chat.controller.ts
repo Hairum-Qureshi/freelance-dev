8a595dacaecfcdec245c65475892f4ada488d45e
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-18 00:05:31 -0400

    created POST endpoint for creating a new chat

--- apps/backend/src/chat/chat.controller.ts
+++ apps/backend/src/chat/chat.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateChatDTO } from '../DTOs/chat.dto';
+import { ChatService } from './chat.service';
+import { CurrentUser } from 'src/decorators/currentUser.decorator';
+import type { UserPayload } from '@repo/shared-types';
 
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
+
+  @Post('create')
+  @UseGuards(AuthGuard())
+  // TODO - need to add a role guard that ensures only hirers can create a chat with potential clients
+  createChat(
+    @Body() createChatDTO: CreateChatDTO,
+    @CurrentUser() currentUser: UserPayload,
+  ) {
+    return this.chatService.createChat(createChatDTO, BigInt(currentUser.id));
+  }
+}
