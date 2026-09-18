aa4998862285ebc65aeccb70c9abd52f96ef96e3
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-17 20:46:34 -0400

    imported NeonDBProvider

--- apps/backend/src/chat/chat.module.ts
+++ apps/backend/src/chat/chat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { NeonDBProvider } from 'src/providers/postgres-db';
 
 @Module({
-  providers: [ChatService],
-  controllers: [ChatController]
+  providers: [ChatService, NeonDBProvider],
+  controllers: [ChatController],
 })
 export class ChatModule {}
