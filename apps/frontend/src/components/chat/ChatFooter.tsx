821c1c7708a321a1c3a1b1db5f0c2a980ec41e0b
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-20 01:21:36 -0400

    changed Database type to schema

--- apps/frontend/src/components/chat/ChatFooter.tsx
+++ apps/frontend/src/components/chat/ChatFooter.tsx
@@ -1,20 +1,27 @@
 import { IoMdAttach } from "react-icons/io";
 import { BsFillSendFill } from "react-icons/bs";
 import Attachment from "./Attachment";
+import { useState } from "react";
+import useChat from "../../hooks/useChat";
 
 export default function ChatFooter() {
+	const [message, setMessage] = useState("");
+	const { createChatMutation } = useChat();
+
 	return (
 		<div className="flex min-h-24 shrink-0 flex-col border-t border-slate-200 bg-white">
-			<div className="px-4 pt-3">
+			{/* <div className="px-4 pt-3">
 				<div className="flex gap-2 overflow-x-auto">
 					<Attachment />
 					<Attachment />
 				</div>
-			</div>
+			</div> */}
 			<div className="flex min-h-20">
 				<textarea
 					className="flex-1 resize-none p-4 outline-none"
 					placeholder="Type a message..."
+					value={message}
+					onChange={e => setMessage(e.target.value)}
 				/>
 
 				<div className="flex w-16 flex-col items-center justify-between p-3 space-y-3">
@@ -27,6 +34,15 @@ export default function ChatFooter() {
 					<button
 						type="button"
 						className="flex h-9 w-9 items-center justify-center rounded-md bg-black text-white transition-colors hover:cursor-pointer hover:bg-slate-800"
+						onClick={() => {
+							if (!message.trim()) {
+								alert("Please enter a message before sending.");
+								return;
+							}
+
+							createChatMutation.mutate({ message });
+							setMessage("");
+						}}
 					>
 						<BsFillSendFill className="text-base" />
 					</button>
