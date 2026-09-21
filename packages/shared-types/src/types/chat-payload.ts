545589eee3a5e11067e979d8d99ecf6c7392f991
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-21 18:31:52 -0400

    removed messages from ChatPayload type

--- packages/shared-types/src/types/chat-payload.ts
+++ packages/shared-types/src/types/chat-payload.ts
@@ -1,8 +1,6 @@
-import { Message } from "./message.js";
 import { Participant } from "./participant.js";
 
 export type ChatPayload = {
 	id: string;
 	participants: { user: Participant }[];
-	messages: Message[];
 };
