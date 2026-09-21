a8857c26a26b215acaefd7fea4c6f43da9c27e95
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-21 18:27:00 -0400

    added onboardingAnswers as part of Participant type

--- packages/shared-types/src/types/participant.ts
+++ packages/shared-types/src/types/participant.ts
@@ -1,6 +1,9 @@
+import { OnboardingAnswers } from "./onboarding-answers.js";
+
 export type Participant = {
 	id: string;
 	profilePicture: string;
 	firstName: string;
 	lastName: string;
+	onboardingAnswers: OnboardingAnswers;
 };
