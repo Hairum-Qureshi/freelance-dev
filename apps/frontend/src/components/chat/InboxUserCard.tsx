ffcec4a9181812a07bd7aa4c26f608f4b05caffb
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-21 15:50:05 -0400

    replaced hardcoded data with dyanmic data

--- apps/frontend/src/components/chat/InboxUserCard.tsx
+++ apps/frontend/src/components/chat/InboxUserCard.tsx
@@ -1,21 +1,41 @@
-export default function InboxUserCard({ selected }: { selected: boolean }) {
+import type { Participant } from "@repo/shared-types";
+import { useCurrentUser } from "../../hooks/useCurrentUser";
+import { Link } from "react-router-dom";
+
+export default function InboxUserCard({
+	selected,
+	chatId,
+	participants
+}: {
+	selected: boolean;
+	chatId: string;
+	participants: { user: Participant }[];
+}) {
+	const { data: currUser } = useCurrentUser();
+
+	const participant = participants.find(
+		p => p.user.id !== currUser?.id && p.user.id !== "1"
+	)?.user;
+
 	return (
-		<div
-			className={`p-3 border-t border-b border-slate-200 cursor-pointer hover:bg-slate-100 ${selected ? "bg-slate-200" : ""}`}
-		>
-			<div className="flex items-center">
-				<div className="h-10 w-10 bg-gray-300 rounded-full mr-3">
-					<img
-						src="https://i.pinimg.com/236x/1d/ec/e2/1dece2c8357bdd7cee3b15036344faf5.jpg?nii=t"
-						alt="User Avatar"
-						className="h-10 w-10 rounded-full"
-					/>
-				</div>
-				<div className="flex-1">
-					<h4 className="font-semibold">User Name</h4>
-					<p className="text-sm text-gray-500">Last message preview...</p>
+		<Link to={`/inbox/c/${chatId}`}>
+			<div
+				className={`p-3 border-t border-b border-slate-200 cursor-pointer hover:bg-slate-100 ${selected ? "bg-slate-200" : ""}`}
+			>
+				<div className="flex items-center">
+					<div className="h-10 w-10 bg-gray-300 rounded-full mr-3">
+						<img
+							src={participant?.profilePicture}
+							alt="User Avatar"
+							className="h-10 w-10 rounded-full"
+						/>
+					</div>
+					<div className="flex-1">
+						<h4 className="font-semibold">{`${participant?.firstName} ${participant?.lastName ?? ""}`}</h4>
+						<p className="text-sm text-gray-500">Last message preview...</p>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
