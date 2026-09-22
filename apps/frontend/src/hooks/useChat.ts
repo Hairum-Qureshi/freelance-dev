545553c01983efe8f6c951f4e1e8cdbd70f48989
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-22 00:42:57 -0400

    added mutation for sending a message

--- apps/frontend/src/hooks/useChat.ts
+++ apps/frontend/src/hooks/useChat.ts
@@ -1,7 +1,7 @@
 import { useParams, useSearchParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import type { ChatPayload } from "@repo/shared-types";
+import type { ChatPayload, Message } from "@repo/shared-types";
 
 export default function useChat() {
 	const queryClient = useQueryClient();
@@ -28,7 +28,35 @@ export default function useChat() {
 				queryKey: ["chats"]
 			});
 
+			queryClient.invalidateQueries({
+				queryKey: ["messages", chatID]
+			});
+
 			navigate(`/inbox/c/${chatID}`);
+		}
+	});
+
+	const createMessageMutation = useMutation({
+		mutationFn: async ({ message }: { message: string }): Promise<void> => {
+			if (!message.trim()) {
+				alert("Message cannot be empty");
+				return;
+			}
+
+			await axios.post(
+				`${import.meta.env.VITE_BACKEND_URL}/api/chat/${chatID}/message`,
+				{
+					message
+				},
+				{
+					withCredentials: true
+				}
+			);
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ["messages", chatID]
+			});
 		}
 	});
 
@@ -45,5 +73,25 @@ export default function useChat() {
 		}
 	});
 
-	return { createChatMutation, currUserChats };
+	const { data: chatMessages } = useQuery({
+		queryKey: ["messages", chatID],
+		queryFn: async () => {
+			if (!chatID) return [];
+
+			const response = await axios.get<Message[]>(
+				`${import.meta.env.VITE_BACKEND_URL}/api/chat/${chatID}`,
+				{
+					withCredentials: true
+				}
+			);
+			return response.data;
+		}
+	});
+
+	return {
+		createChatMutation,
+		createMessageMutation,
+		currUserChats,
+		chatMessages
+	};
 }
