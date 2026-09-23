ddfa3a3f5e9e1091c8f35e9b3d7afaeb6fcdc28e
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-22 21:02:58 -0400

    added ImageKitProvider to providers array

--- apps/backend/src/chat/chat.module.ts
+++ apps/backend/src/chat/chat.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { NeonDBProvider } from 'src/providers/postgres-db';
+import { ImageKitProvider } from 'src/providers/imagekit';
 
 @Module({
-  providers: [ChatService, NeonDBProvider],
+  providers: [ChatService, NeonDBProvider, ImageKitProvider],
   controllers: [ChatController],
 })
 export class ChatModule {}
