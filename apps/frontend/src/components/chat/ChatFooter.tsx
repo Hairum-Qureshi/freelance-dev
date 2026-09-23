0e5a03fe60fee346f0168cb5f25e8ebd7a784e60
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-22 20:41:44 -0400

    added code for listing files and passing them into Attachment component

--- apps/frontend/src/components/chat/ChatFooter.tsx
+++ apps/frontend/src/components/chat/ChatFooter.tsx
@@ -1,22 +1,40 @@
 import { IoMdAttach } from "react-icons/io";
 import { BsFillSendFill } from "react-icons/bs";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 import useChat from "../../hooks/useChat";
+import Attachment from "./Attachment";
 
 export default function ChatFooter() {
 	const [message, setMessage] = useState("");
 	const { createChatMutation, createMessageMutation } = useChat();
 	const [searchParams] = useSearchParams();
+	const fileRef = useRef<HTMLInputElement | null>(null);
+	const [attachedFiles, setAttachedFiles] = useState<File[]>([]);
+
+	function attachFiles(e: React.ChangeEvent<HTMLInputElement>) {
+		if (e.target.files) {
+			console.log("Attaching files:", e.target.files);
+			setAttachedFiles([...attachedFiles, ...Array.from(e.target.files)]);
+		}
+	}
 
 	return (
 		<div className="flex min-h-24 shrink-0 flex-col border-t border-slate-200 bg-white">
-			{/* <div className="px-4 pt-3">
-				<div className="flex gap-2 overflow-x-auto">
-					<Attachment />
-					<Attachment />
+			{attachedFiles.length > 0 && (
+				<div className="px-4 pt-3">
+					<div className="flex gap-2 overflow-x-auto">
+						{attachedFiles.map((file, index) => (
+							<Attachment
+								key={index}
+								fileType={file.type}
+								fileName={file.name}
+								fileSize={file.size}
+							/>
+						))}
+					</div>
 				</div>
-			</div> */}
+			)}
 			<div className="flex min-h-20">
 				<textarea
 					className="flex-1 resize-none p-4 outline-none"
@@ -26,9 +44,23 @@ export default function ChatFooter() {
 				/>
 
 				<div className="flex w-16 flex-col items-center justify-between p-3 space-y-3">
+					<input
+						type="file"
+						className="hidden"
+						id="file-input"
+						ref={fileRef}
+						multiple
+						accept="image/*,application/pdf"
+						onChange={() =>
+							attachFiles({
+								target: { files: fileRef.current?.files }
+							} as React.ChangeEvent<HTMLInputElement>)
+						}
+					/>
 					<button
 						type="button"
 						className="text-slate-400 transition-colors hover:cursor-pointer hover:text-black"
+						onClick={() => fileRef.current?.click()}
 					>
 						<IoMdAttach className="text-2xl" />
 					</button>
