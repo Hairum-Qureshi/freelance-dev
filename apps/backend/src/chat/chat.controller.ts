257bb8bdab5bfad815643a9ee32d434e1f565077
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-22 20:39:51 -0400

    modified POST chat message endpoint to handle optional file uploads as well

--- apps/backend/src/chat/chat.controller.ts
+++ apps/backend/src/chat/chat.controller.ts
@@ -4,10 +4,14 @@ import { CreateChatDTO } from '../DTOs/chat.dto';
 import { ChatService } from './chat.service';
 import { CurrentUser } from 'src/decorators/currentUser.decorator';
 import type { UserPayload } from '@repo/shared-types';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { UseInterceptors } from '@nestjs/common';
 
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
+
+  // TODO - need to block users from sending/starting a chat with the ADMIN user
 
   @Post('create')
   @UseGuards(AuthGuard())
@@ -33,11 +37,18 @@ export class ChatController {
 
   @Post(':chatId/message')
   @UseGuards(AuthGuard())
+  @UseInterceptors(FilesInterceptor('attachments', 5))
   addMessage(
     @CurrentUser() currentUser: UserPayload,
     @Param('chatId') chatId: string,
     @Body('message') message: string,
+    @Body('attachments') attachments?: Express.Multer.File[],
   ) {
-    return this.chatService.addMessage(chatId, currentUser.id, message);
+    return this.chatService.addMessage(
+      chatId,
+      currentUser.id,
+      message,
+      attachments,
+    );
   }
 }
