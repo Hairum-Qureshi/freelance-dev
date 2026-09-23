ac5c0142c335daeb5855d6670f8be4ae197a71fa
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-23 12:30:33 -0400

    added latestMessage to ChatPayload type

--- packages/shared-types/src/types/chat-payload.ts
+++ packages/shared-types/src/types/chat-payload.ts
@@ -2,5 +2,11 @@ import { Participant } from "./participant.js";
 
 export type ChatPayload = {
 	id: string;
+	latestMessage: {
+		id: string;
+		message: string;
+		createdAt: Date;
+		updatedAt: Date;
+	};
 	participants: { user: Participant }[];
 };
