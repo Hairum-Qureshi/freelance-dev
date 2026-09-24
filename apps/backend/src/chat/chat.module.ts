9f0e9b3ff3c7a28e8d9c6b0aaa5478c087ed965e
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-23 23:10:10 -0400

    added EmailModule to imports array

--- apps/backend/src/chat/chat.module.ts
+++ apps/backend/src/chat/chat.module.ts
@@ -3,9 +3,11 @@ import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { NeonDBProvider } from 'src/providers/postgres-db';
 import { ImageKitProvider } from 'src/providers/imagekit';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
   providers: [ChatService, NeonDBProvider, ImageKitProvider],
   controllers: [ChatController],
+  imports: [EmailModule],
 })
 export class ChatModule {}
