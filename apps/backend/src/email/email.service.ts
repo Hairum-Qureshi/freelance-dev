d48c76e03705d4a0b16772fbd0720ff1b18331d9
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-24 01:15:38 -0400

    wrote method for emailing user when a new user starts a conversation with them

--- apps/backend/src/email/email.service.ts
+++ apps/backend/src/email/email.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { RESEND_CLIENT } from '../providers/resend';
+import { Resend } from 'resend';
+import { ConfigService } from '@nestjs/config';
+import { EMAIL_TEMPLATE } from './email-template';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  constructor(
+    @Inject(RESEND_CLIENT) private readonly resend: Resend,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async sendNewContactMessageNotifEmail(
+    to: string,
+    fullName: string,
+    timestamp: string,
+    message: string,
+    initials: string,
+    messageUrl: string,
+  ) {
+    return this.resend.emails.send({
+      from: `Freelance Dev <${this.configService.getOrThrow<string>('EMAIL_FROM')}>`,
+      to,
+      subject: 'New Contact Message',
+      html: EMAIL_TEMPLATE.replace('{{full name}}', fullName)
+        .replace('{{ email }}', to)
+        .replace('{{ initials }}', initials)
+        .replace('{{ timestamp }}', timestamp)
+        .replace('{{ message }}', message)
+        .replace('{{ message_url }}', messageUrl),
+    });
+  }
+}
