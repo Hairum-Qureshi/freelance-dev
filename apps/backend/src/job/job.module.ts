e576b0719cb3a1880978664bf8ab58b7d045b8c2
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-24 15:32:56 -0400

    added NeonDBProvider to providers array

--- apps/backend/src/job/job.module.ts
+++ apps/backend/src/job/job.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JobService } from './job.service';
 import { JobController } from './job.controller';
+import { NeonDBProvider } from 'src/providers/postgres-db';
 
 @Module({
-  providers: [JobService],
-  controllers: [JobController]
+  providers: [JobService, NeonDBProvider],
+  controllers: [JobController],
 })
 export class JobModule {}
