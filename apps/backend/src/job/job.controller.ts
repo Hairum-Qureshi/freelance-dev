78f81bc13e422f1f9589ca663d832fa891165572
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-24 15:44:48 -0400

    added POST endpoint for creating a job

--- apps/backend/src/job/job.controller.ts
+++ apps/backend/src/job/job.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { JobPostingDTO } from 'src/DTOs/job.dto';
+import { JobService } from './job.service';
+import { CurrentUser } from 'src/decorators/currentUser.decorator';
+import type { UserPayload } from '@repo/shared-types';
 
 @Controller('job')
-export class JobController {}
+export class JobController {
+  constructor(private readonly jobService: JobService) {}
+
+  @Post('create')
+  @UseGuards(AuthGuard())
+  async createJob(
+    @Body() jobPostingDTO: JobPostingDTO,
+    @CurrentUser() currentUser: UserPayload,
+  ) {
+    return this.jobService.createJob(jobPostingDTO, currentUser.id);
+  }
+}
