14097537df3b6a575c4812b82a50f0d55232dded
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-24 22:41:21 -0400

    created JobPayload type

---
+++ packages/shared-types/src/types/job-payload.ts
@@ -0,0 +1,28 @@
+export type JobPayload = {
+	id: string;
+	jobTitle: string;
+	businessName: string;
+	projectType: string;
+	lookingFor: string;
+	experienceLevel: string;
+	jobType: string;
+	paymentType: string;
+	workLocation: string;
+	region: string;
+	timeline: string;
+	projectDetails: string;
+	deliverables: string;
+	budgetMin: number;
+	budgetMax: number;
+	skills: string[];
+	posterId: string;
+	poster: {
+		id: string;
+		firstName: string;
+		lastName: string;
+		profilePicture: string;
+		email: string;
+	};
+	createdAt: Date;
+	updatedAt: Date;
+};
