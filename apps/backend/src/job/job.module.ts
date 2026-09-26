da5de780a60d09b19ab4eb2dbedba582c1ba3485
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-25 21:59:29 -0400

    added EmailModule to imports array

--- apps/backend/src/job/job.module.ts
+++ apps/backend/src/job/job.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { JobService } from './job.service';
 import { JobController } from './job.controller';
 import { NeonDBProvider } from 'src/providers/postgres-db';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
   providers: [JobService, NeonDBProvider],
   controllers: [JobController],
+  imports: [EmailModule],
 })
 export class JobModule {}
