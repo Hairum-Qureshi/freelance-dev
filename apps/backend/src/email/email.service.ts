ef2c883aaaff07aa9bec526a627159012d449daa
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-25 22:04:20 -0400

    moved email templates into folder

--- apps/backend/src/email/email.service.ts
+++ apps/backend/src/email/email.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Inject } from '@nestjs/common';
 import { RESEND_CLIENT } from '../providers/resend';
 import { Resend } from 'resend';
 import { ConfigService } from '@nestjs/config';
-import { EMAIL_TEMPLATE } from './email-template';
+import { EMAIL_TEMPLATE } from './templates/new-contact-message';
 
 @Injectable()
 export class EmailService {
@@ -29,6 +29,21 @@ export class EmailService {
         .replace('{{ timestamp }}', timestamp)
         .replace('{{ message }}', message)
         .replace('{{ message_url }}', messageUrl),
+    });
+  }
+
+  async sendApplicationStatusUpdateEmail(
+    to: string,
+    applicantName: string,
+    status: 'accepted' | 'rejected',
+  ) {
+    return this.resend.emails.send({
+      from: `Freelance Dev <${this.configService.getOrThrow<string>('EMAIL_FROM')}>`,
+      to,
+      subject: 'Application Status Update',
+      html: EMAIL_TEMPLATE.replace('{{full name}}', applicantName)
+        .replace('{{ email }}', to)
+        .replace('{{ status }}', status),
     });
   }
 }
