4fa5aea89a17ba1db09961d2ea98bb57288df4ab
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-25 22:50:18 -0400

    added conditonal where if the user has already updated a job application status, they can retract it

--- apps/frontend/src/components/ApplicationDetails.tsx
+++ apps/frontend/src/components/ApplicationDetails.tsx
@@ -3,16 +3,20 @@ import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { FaCheck } from "react-icons/fa6";
 import { TbCancel } from "react-icons/tb";
+import useApplication from "../hooks/useApplication";
 
 export default function ApplicationDetails({
-	application
+	application,
+	setSelectedApplicationId
 }: {
 	application: ApplicationPayload;
+	setSelectedApplicationId: (id: string | null) => void;
 }) {
 	const { applicant } = application;
 	const resumeUrl = applicant.resumeId
 		? `${import.meta.env.VITE_IMAGE_KIT_URL_ENDPOINT}/profiles/${applicant.id}/Resume.pdf?v=${applicant.resumeId}`
 		: null;
+	const { updateApplicantStatusMutation } = useApplication();
 
 	return (
 		<div className="px-6 py-5">
@@ -56,34 +60,88 @@ export default function ApplicationDetails({
 			</section>
 			<section className="border-t border-slate-200 pt-5">
 				<div className="divide-y divide-slate-200 rounded-md border border-slate-200">
-					<div className="flex items-center justify-between bg-gradient-to-r from-transparent to-green-50 px-4 py-3 transition-colors duration-200 hover:to-green-200">
-						<span className="text-sm font-medium text-slate-700">
-							Accept Application
-						</span>
+					{application.status === "pending" ? (
+						<>
+							<div className="flex items-center justify-between bg-gradient-to-r from-transparent to-green-50 px-4 py-3 transition-colors duration-200 hover:to-green-200">
+								<span className="text-sm font-medium text-slate-700">
+									Accept Application
+								</span>
 
-						<button
-							className="flex h-8 w-8 items-center justify-center rounded-md text-green-600 transition-colors duration-200 hover:cursor-pointer hover:bg-green-100"
-							aria-label="Accept application"
-						>
-							<FaCheck aria-hidden="true" className="text-xl" />
-						</button>
-					</div>
-
-					<div className="flex items-center justify-between bg-gradient-to-r from-transparent to-red-50 px-4 py-3 transition-colors duration-200 hover:to-red-200">
-						<span className="text-sm font-medium text-slate-700">
-							Reject Application
-						</span>
-
-						<button
-							className="flex h-8 w-8 items-center justify-center rounded-md text-red-600 transition-colors duration-200 hover:cursor-pointer hover:bg-red-100"
-							aria-label="Reject application"
-						>
-							<TbCancel aria-hidden="true" className="text-lg" />
-						</button>
-					</div>
+								<button
+									className="flex h-8 w-8 items-center justify-center rounded-md text-green-600 transition-colors duration-200 hover:cursor-pointer hover:bg-green-100"
+									aria-label="Accept application"
+									onClick={() => {
+										const confirmation = window.confirm(
+											"Are you sure you want to accept this application?"
+										);
+										if (!confirmation) return;
+										updateApplicantStatusMutation.mutate({
+											applicationId: application.id,
+											status: "accepted",
+											applicantName: `${applicant.firstName} ${applicant.lastName}`,
+											applicantEmail: applicant.email,
+											jobTitle: application.job.jobTitle
+										});
+										setSelectedApplicationId(null);
+									}}
+								>
+									<FaCheck aria-hidden="true" className="text-xl" />
+								</button>
+							</div>
+							<div className="flex items-center justify-between bg-gradient-to-r from-transparent to-red-50 px-4 py-3 transition-colors duration-200 hover:to-red-200">
+								<span className="text-sm font-medium text-slate-700">
+									Reject Application
+								</span>
+								<button
+									className="flex h-8 w-8 items-center justify-center rounded-md text-red-600 transition-colors duration-200 hover:cursor-pointer hover:bg-red-100"
+									aria-label="Reject application"
+									onClick={() => {
+										const confirmation = window.confirm(
+											"Are you sure you want to reject this application?"
+										);
+										if (!confirmation) return;
+										updateApplicantStatusMutation.mutate({
+											applicationId: application.id,
+											status: "rejected",
+											applicantName: `${applicant.firstName} ${applicant.lastName}`,
+											applicantEmail: applicant.email,
+											jobTitle: application.job.jobTitle
+										});
+										setSelectedApplicationId(null);
+									}}
+								>
+									<TbCancel aria-hidden="true" className="text-lg" />
+								</button>
+							</div>
+						</>
+					) : (
+						<div className="px-4 py-3 text-sm text-slate-700">
+							This application has been {application.status}.
+							<button
+								className="ml-2 rounded-md hover:cursor-pointer bg-blue-500 px-3 py-1 text-sm text-white hover:bg-blue-600"
+								onClick={() => {
+									const confirmation = window.confirm(
+										"Are you sure you want to retract this application?"
+									);
+									if (!confirmation) return;
+									updateApplicantStatusMutation.mutate({
+										applicationId: application.id,
+										status: "pending",
+										applicantName: `${applicant.firstName} ${applicant.lastName}`,
+										applicantEmail: applicant.email,
+										jobTitle: application.job.jobTitle
+									});
+									setSelectedApplicationId(null);
+								}}
+							>
+								Retract
+							</button>
+						</div>
+					)}
 				</div>
 				<p className="py-3 text-sm text-slate-500">
-					{applicant.firstName} will automatically be notified of your decision via email.
+					{applicant.firstName} will automatically be notified of your decision
+					via email.
 				</p>
 			</section>
 		</div>
